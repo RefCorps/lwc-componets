a722709a89ac12f997ebddeb2044191c53c59c6f
Mike Miller <mikemiller@tawmrm.com>
2021-01-27 07:51:12 -0500

    Used in Flow:  Referee Status, Rank, and Designation - V10

---
+++ force-app/main/default/lwc/ckbRefereeStatus/ckbRefereeStatus.js
@@ -0,0 +1,69 @@
+import { api, LightningElement, wire } from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import REFEREE_STATUS from '@salesforce/schema/Contact.Referee_Status__c';
+import {FlowAttributeChangeEvent } from 'lightning/flowSupport';
+
+export default class ckbRefereeStatus extends LightningElement {
+
+    @api currentStatus;
+
+    get getCurrentStatus() {
+        return this.currentStatus;
+    }
+
+    @api selectedStatus;
+    @api excludeStatuses;
+    @api checkboxLabel;
+
+    @api errorLabel;    // used to loop back an display an error
+
+    editedPicklistValues = [];
+    value;
+    error;
+    checkboxGroupLabel;
+
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: REFEREE_STATUS })
+ 
+    wiredValues({ error, data }) {
+
+        this.selectedStatus = this.currentStatus;
+
+        // test test test uncomment to preview component locally 
+        // this.excludeStatuses = this.excludeStatuses ? this.excludeStatuses : 'License Suspended,License Suspended - Safety Compliance,License Suspended - USRowing Admin';
+
+        // console.log("In wiredValues " + data);
+        if (data) {
+            this.editedPicklistValues = this.buildSelectOptions(data.values);
+            this.error = undefined;
+        } else {
+            this.error = error;
+            this.treeModel = undefined;
+        }
+        this.checkboxGroupLabel = this.checkboxLabel ? this.checkboxLabel : 'Referee Status';
+
+    }
+
+    buildSelectOptions(picklistValues) {
+
+        const listValues = []; 
+        Object.keys(picklistValues).forEach((index) => {
+
+            if(!this.excludeStatuses.includes( picklistValues[index].label )) {
+                listValues.push( {label: picklistValues[index].label, value: picklistValues[index].value} );
+            }
+
+        });
+        
+        this.value = this.currentStatus;
+        return listValues;
+    }
+
+    handleChange(event) {
+        this.selectedStatus = event.target.value; 
+        const attributeChangeEvent = new FlowAttributeChangeEvent('ckboxGroup', this.selectedStatus);
+        this.dispatchEvent(attributeChangeEvent);   
+        // console.log("currentStatus1="+this.currentStatus);
+    }
+
+
+}
--- force-app/main/default/lwc/tableDesignations/tableDesignations.js
+++ force-app/main/default/lwc/tableDesignations/tableDesignations.js
@@ -1,123 +1,174 @@
-import { api, LightningElement, wire, track } from 'lwc';
+import {
+    api,
+    LightningElement,
+    wire,
+    track
+} from 'lwc';
 
-import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import {
+    FlowAttributeChangeEvent
+} from 'lightning/flowSupport';
 
 // NOTE:  For Apex imports, don't use curly braces, also you can designate a defaultExport name that's different from the module-name
 import ContactDesignations from '@salesforce/apex/DesignationController.fetchContactDesignations';
 import DesignationPicklist from '@salesforce/apex/DesignationController.getDesignationsPicklist';
 
-const columns = [
-    {label: 'Rec Name', fieldName: 'Name', type: 'text'},
-    {label: 'Designation', fieldName: 'Designation__c', type: 'text'},
-    {label: 'Start Date', fieldName: 'Designation_Start_Date__c', type: 'date'},
-    {label: 'End Date', fieldName: 'Designation_End_Date__c', type: 'date'}
-]
+const columns = [{ label: 'Rec Name',fieldName: 'Name',type: 'text' },
+    { label: 'Designation',fieldName: 'Designation__c',type: 'text' },
+    { label: 'Start Date',fieldName: 'Designation_Start_Date__c',type: 'date' },
+    { label: 'End Date',fieldName: 'Designation_End_Date__c',type: 'date' } ]
 
 export default class testWithApexDataSource extends LightningElement {
 
-   @api designeeId 
-   @api currentDesignation
+    @api designeeId
+    @api selectedDesignation
+    @api selectionStartDate
+    @api selectionEndDate
 
-   @api selectLabel;
-   @api errorLabel;
 
-   data;
-   columns = columns;
+    @api selectLabel;
+    @api selectHelp;
+    @api tableLabel;
+    @api tableHelp;
+    // @api errorLabel;
 
-   mydesignations = [];
-   editedPicklistValues = [];
-   picklist;
+    pDesigneeId;
 
-   pDesigneeId;
+    data;
+    columns = columns;
 
-   loadPicklist = false;
+    mydesignations = [];
+    editedPicklistValues = [];
+    picklist;
 
-   hidenseek = "display:none";
+    lblSelect;
+    infoSelect ="Current designations are displayed. Candidates and Assistants will have one designation equal to rank.  Referees may have several as well as rank.";
+    lblTable;
+    infoTable = "Designations are similar to rank, but are not the same.  A member of the corps may have several designations.";
 
-   @track inputValue;
-   // initialize component
-   async connectedCallback() {
+    loadPicklist = false;
 
-       //alert("designeeId = " + this.designeeId);
-       this.pDesigneeId = this.designeeId ? this.designeeId : '0036A00000peZM1QAM';
-       //alert("pDesigneeId = " + this.pDesigneeId);
+    hidenseek = "display:none";
 
-       //alert("In connectedCallback fetchContactDesignations for " + this.pDesigneeId);
-       // NOTE: WHE
-       ContactDesignations({ pDesigneeId: this.pDesigneeId })
-        .then(result=>{
-            this.data = result;
-            console.log('result:  ' + JSON.stringify(result) );
-            var d;
-            for (d of this.data) {
-                this.mydesignations.push(d.Designation__c);
-            }
-        }).then(result=>{
+    todaysDate;
 
-            // Loading the picklist is dependent upon first fetcing current designations
-            // refer to chained promises
-        
-            DesignationPicklist().then(result=>{
-            this.picklist = result;
-            // console.log('result:  ' + JSON.stringify(result) );
+    @track inputValue;
+    // initialize component
+    async connectedCallback() {
 
-            // test test test
-            this.editedPicklistValues.push('-- SELECT A NEW DESIGNATION --');
+        //alert("designeeId = " + this.designeeId);
+        this.pDesigneeId = this.designeeId ? this.designeeId : '0036A00000peZM1QAM';
+        //alert("pDesigneeId = " + this.pDesigneeId);
 
-            var dx;
-            var max = 0;
-            for(dx of this.picklist) {
-                if(dx === 'Assistant Referee') {
-                    if(max < 1) {
-                        max = 1;
-                    }    
-                };
-                if(dx === 'Full Referee') {
-                    if(max < 2) {
-                        max = 2;
-                    }    
-                };
-            }
-            //alert("max="+ max);
-            var dl;
-            for (dl of this.picklist) {
+        this.lblSelect = this.selectLabel ? this.selectLabel : 'New Designations';
+        this.lblTable = this.tableLabel ? this.tableLabel : 'Current Designations';
 
-                if(max == 1) {
-                    this.editedPicklistValues.push('Assistant Referee');
-                    break;
-                } else if(max == 2) {
-                    this.editedPicklistValues.push('Full Referee');
-                    break;
-                } 
-                if(!this.mydesignations.includes(dl)) {
-                    this.editedPicklistValues.push(dl);
-                } 
-                //else {
-                //    this.editedPicklistValues.push(dl);
-                //}
-            }
-            this.loadPicklist = true;
-            
-            // TODO:  THIS WORKS LOCALLY BUT WILL NOT DEPLOY, IT IS NOT RECOMMEDED updateRecord IS NOT WORKING FOR ME
-            //eval($A.get('e.force:refreshView'));
-            // updateRecord({fields: this.picklist});
+        this.infoSelect = this.selectHelp ? this.selectHelp : this.infoSelect;
+        this.infoTable = this.selectTable ? this.tableHelp : this.infoTable;
 
-            }).catch(error=>{
-                alert('picklist error:  ' + error);
-            });
-        
-        }).catch(error=>{
+        this.todaysDate = this.getDateYYYYMMDD();
+
+        //alert("In connectedCallback fetchContactDesignations for " + this.pDesigneeId);
+        // NOTE: WHE
+        ContactDesignations({
+                pDesigneeId: this.pDesigneeId
+            })
+            .then(result => {
+                this.data = result;
+                console.log('Designations:  ' + JSON.stringify(result));
+                var d;
+                for (d of this.data) {
+                    this.mydesignations.push(d.Designation__c);
+                }
+            }).then(result => {
+
+                // Loading the picklist is dependent upon first fetcing current designations
+                // refer to chained promises
+
+                DesignationPicklist().then(result => {
+                    this.picklist = result;
+                    console.log('Picklist:  ' + JSON.stringify(result) );
+
+                    // test test test
+                    this.editedPicklistValues.push('-- SELECT A NEW DESIGNATION --');
+
+                    var dl;
+                    var md;
+                    var dx = 0;
+                    for (md of this.mydesignations) {
+                        if (md === 'Assistant Referee') {
+                            if (dx < 1) {
+                                dx = 1;
+                                console.log("md=" + md + " dx=" + dx );
+                            }
+                        }
+                        if (md === 'Full Referee') {
+                            if (dx < 2) {
+                                dx = 2;
+                                console.log("md=" + md + " dx=" + dx );
+                            }
+                        }
+                    }
+                    console.log("dx="+ dx);
+                    //var dl;
+                    for (dl of this.picklist) {
+
+                        if (dx == 0) {
+                            this.editedPicklistValues.push('Assistant Referee');
+                            break;
+                        } else if (dx == 1) {
+                            this.editedPicklistValues.push('Full Referee');
+                            break;
+                        }
+                        if (!this.mydesignations.includes(dl)) {
+                            this.editedPicklistValues.push(dl);
+                        }
+                        //else {
+                        //    this.editedPicklistValues.push(dl);
+                        //}
+                    }
+                    this.loadPicklist = true;
+
+                    // TODO:  THIS WORKS LOCALLY BUT WILL NOT DEPLOY, IT IS NOT RECOMMEDED updateRecord IS NOT WORKING FOR ME
+                    //eval($A.get('e.force:refreshView'));
+                    // updateRecord({fields: this.picklist});
+
+                }).catch(error => {
+                    alert('picklist error:  ' + error);
+                });
+
+            }).catch(error => {
                 alert('detail error:  ' + error);
-        });
+            });
 
 
-   }
-
-   handleChange(event) {
-        this.selectedDesignation = event.target.value; 
-        const attributeChangeEvent = new FlowAttributeChangeEvent('select', this.selectedDesignation);
-        this.dispatchEvent(attributeChangeEvent);   
-        this.hidenseek = "display:inline";
     }
+
+    handleSelection(event) {
+        this.selectedDesignation = event.target.value;
+        // DOTO: this is supposed to change date from hidden to visible - not configured correctly
+        const attributeChangeEvent = new FlowAttributeChangeEvent('select', this.selectedDesignation);
+        this.dispatchEvent(attributeChangeEvent);
+        this.hidenseek = "display:inline";
+        this.selectionStartDate = this.getDateYYYYMMDD();
+        console.log("default startdate " + this.selectionStartDate);
+    }
+
+    handleStartDate(event) {
+        this.selectionStartDate = event.target.value;
+        console.log("selected startdate " + this.selectionStartDate);
+    }
+
+    getDateYYYYMMDD() {
+        var today = new Date();
+        var p = '-';
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; 
+        var yyyy = today.getFullYear();
+        dd = dd<10 ? '0'+dd : dd;  
+        mm = mm<10 ? '0'+mm : mm;
+        return yyyy+p+mm+p+dd;
+    }
+
 
 }
